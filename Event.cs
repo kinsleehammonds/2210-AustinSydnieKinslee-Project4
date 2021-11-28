fbbe517d57a7c2341957fa4c788fba899d000b07
Austinhamilton1 <95155318+Austinhamilton1@users.noreply.github.com>
2021-11-28 11:03:13 -0500

    Add files via upload

---
+++ Event.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresProject4
+{
+    enum EVENTTYPE { ENTER, LEAVE };
+    class Event : IComparable
+    {
+        public EVENTTYPE Type { get; set; }
+        public DateTime Time { get; set; }
+        public int Customer { get; set; }
+
+        public Event()
+        {
+            Type = EVENTTYPE.ENTER;
+            Time = DateTime.Now;
+            Customer = -1;
+        }
+
+        public Event(EVENTTYPE type, DateTime time, int customer)
+        {
+            Type = type;
+            Time = time;
+            Customer = customer;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Customer {0} {1}s at {2}", Customer, Type, Time);
+        }
+
+        public int CompareTo(Object obj)
+        {
+            if (!(obj is Event))
+                throw new ArgumentException("The argument is not an Event object");
+
+            Event e = (Event)obj;
+            return e.Time.CompareTo(Time);
+        }
+    }
+}
